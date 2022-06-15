135c6cfcd7aff8ea621078e5f6f16e7a3c89bdf1
Макс Кузнецов <orenburg20172017@gmail.com>
2022-06-15 17:39:43 +0300

    исправил билд, дописал еще 2 теста

--- src/test/java/ru/netology/service/CashbackHuckServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHuckServiceTest.java
@@ -15,4 +15,24 @@ public class CashbackHuckServiceTest {
 
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void testRemainOver1000() {
+        CashbackHuckService service = new CashbackHuckService();
+
+        int expected = 200;
+        int actual = service.remain(1800);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIf0() {
+        CashbackHuckService service = new CashbackHuckService();
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(actual, expected);
+    }
 }
